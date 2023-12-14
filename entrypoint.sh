976c0ec688756fef2ebada1a04137d7a772b8b0f
ShotaTanemura <70319686+ShotaTanemura@users.noreply.github.com>
2023-12-14 18:25:30 +0900

    Topic/6 topicentrypoint allows to access to desktop on container (#7)
    * Setup ubuntu22.04 desktop environment
    
    * docker-compose allows starting container simply
    
    * Add packages listed at referred repository
    
    * Setup noVNC
    
    * Enable apt-get compiletion
    
    * Allows execute entrypoint
    
    * entrypoint creates user
    
    * Setup VNC in entrypoint
    
    * Setup xstartup in entrypoint
    
    * entrypoint launch vnc server
    
    * Setup supervisor in entrypoint
    
    * entrypoint add shortcut for terminator
    
    * Clear up sensitive environment values
    
    * entrypoint execute supervisor's script

---
+++ entrypoint.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Create User
+USER=${USER:-root}
+HOME=/root
+if [ "$USER" != "root" ]; then
+    echo "* enable custom user: $USER"
+    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    if [ -z "$PASSWORD" ]; then
+        echo "  set default password to \"ubuntu\""
+        PASSWORD=ubuntu
+    fi
+    HOME=/home/$USER
+    echo "$USER:$PASSWORD" | /usr/sbin/chpasswd 2> /dev/null || echo ""
+    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} ${HOME} 2>/dev/null
+    chown -R $USER:$USER ${HOME}
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+fi
+
+# VNC password
+VNC_PASSWORD=${PASSWORD:-ubuntu}
+
+# Setup VNC
+mkdir -p $HOME/.vnc
+echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
+chmod 600 $HOME/.vnc/passwd
+chown -R $USER:$USER $HOME
+sed -i "s/password = WebUtil.getConfigVar('password');/password = '$VNC_PASSWORD'/" /usr/lib/novnc/app/ui.js
+
+# xstartup
+XSTARTUP_PATH=$HOME/.vnc/xstartup
+cat << EOF > $XSTARTUP_PATH
+#!/bin/sh
+unset DBUS_SESSION_BUS_ADDRESS
+mate-session
+EOF
+chown $USER:$USER $XSTARTUP_PATH
+chmod 755 $XSTARTUP_PATH
+
+# vncserver launch
+VNCRUN_PATH=$HOME/.vnc/vnc_run.sh
+cat << EOF > $VNCRUN_PATH
+#!/bin/sh
+
+if [ $(uname -m) = "aarch64" ]; then
+    LD_PRELOAD=/lib/aarch64-linux-gnu/libgcc_s.so.1 vncserver :1 -fg -geometry 1920x1080 -depth 24
+else
+    vncserver :1 -fg -geometry 1920x1080 -depth 24
+fi
+EOF
+
+# Supervisor
+CONF_PATH=/etc/supervisor/conf.d/supervisord.conf
+cat << EOF > $CONF_PATH
+[supervisord]
+nodaemon=true
+user=root
+[program:vnc]
+command=gosu '$USER' bash '$VNCRUN_PATH'
+[program:novnc]
+command=gosu '$USER' bash -c "websockify --web=/usr/lib/novnc 80 localhost:5901"
+EOF
+
+
+# Add terminator shortcut
+mkdir -p $HOME/Desktop
+cat << EOF > $HOME/Desktop/terminator.desktop
+[Desktop Entry]
+Name=Terminator
+Comment=Multiple terminals in one window
+TryExec=terminator
+Exec=terminator
+Icon=terminator
+Type=Application
+Categories=GNOME;GTK;Utility;TerminalEmulator;System;
+StartupNotify=true
+X-Ubuntu-Gettext-Domain=terminator
+X-Ayatana-Desktop-Shortcuts=NewWindow;
+Keywords=terminal;shell;prompt;command;commandline;
+[NewWindow Shortcut Group]
+Name=Open a New Window
+Exec=terminator
+TargetEnvironment=Unity
+EOF
+
+chown -R $USER:$USER $HOME/Desktop
+
+# clearup
+PASSWORD=
+VNC_PASSWORD=
+
+exec /bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
