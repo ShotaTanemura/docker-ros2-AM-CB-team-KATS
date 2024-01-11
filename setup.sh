4e8dc6a0938ab875fa9c50575a14537b14c3be28
ShotaTanemura <shota0711tane@gmail.com>
2024-01-11 13:13:37 +0900

    Use sudo

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -eu
 
 # Upgrade OS
-apt-get update -q && \
-    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && \
-    apt-get autoclean && \
-    apt-get autoremove && \
-    rm -rf /var/lib/apt/lists/*
+sudo apt-get update -q && \
+    DEBIAN_FRONTEND=noninteractive sudo apt-get upgrade -y && \
+    sudo apt-get autoclean && \
+    sudo apt-get autoremove && \
+    sudo rm -rf /var/lib/apt/lists/*
 
 # Install essential packages
-apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+sudo apt-get update && \
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
         wget curlgit python3-pip \
         build-essential vim lsb-release locales \
         bash-completion tzdata terminator && \
-    apt-get autoclean && \
-    apt-get autoremove && \
-    rm -rf /var/lib/apt/lists/*
+    sudo apt-get autoclean && \
+    sudo apt-get autoremove && \
+    sudo rm -rf /var/lib/apt/lists/*
 
 # Set distro
 ROS_DISTRO='humble'
@@ -25,44 +25,44 @@ INSTALL_PACKAGE='desktop'
 
 # Setup locale
 locale && \
-    apt-get update && apt-get install locales && \
+    sudo apt-get update && sudo apt-get install locales && \
     locale-gen en_US en_US.UTF-8 && \
     update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 && \
     export LANG=en_US.UTF-8 && \
     locale
 
 # Ensure the Ubuntu Universe repository is enabled
-apt-get install -y software-properties-common && \
+sudo apt-get install -y software-properties-common && \
     add-apt-repository universe
 
 # Install ROS2
-apt-get update -q && \
-    apt-get install -y curl gnupg2 lsb-release && \
+sudo apt-get update -q && \
+    sudo apt-get install -y curl gnupg2 lsb-release && \
     curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg && \
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null && \
-    apt-get update -q && \
-    DEBIAN_FRONTEND=noninteractive apt-get upgrade -y && \
-    apt-get install -y ros-${ROS_DISTRO}-${INSTALL_PACKAGE} \
+    sudo apt-get update -q && \
+    DEBIAN_FRONTEND=noninteractive sudo apt-get upgrade -y && \
+    sudo apt-get install -y ros-${ROS_DISTRO}-${INSTALL_PACKAGE} \
     python3-argcomplete \
     python3-colcon-common-extensions \
     python3-rosdep python3-vcstool && \
     rosdep init && \
-    rm -rf /var/lib/apt/lists/*
+    sudo rm -rf /var/lib/apt/lists/*
 
 rosdep update
 
 # Add Google Cartographer and its related packages
-apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+sudo apt-get update && \
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
         ros-humble-cartographer \
         ros-humble-cartographer-rviz && \
-    apt-get autoclean && \
-    apt-get autoremove && \
-    rm -rf /var/lib/apt/lists/*
+    sudo apt-get autoclean && \
+    sudo apt-get autoremove && \
+    sudo rm -rf /var/lib/apt/lists/*
 
 # Install Navigation2
-apt-get update -q && \
-    apt-get install -y \
+sudo apt-get update -q && \
+    sudo apt-get install -y \
     ros-humble-navigation2 ros-humble-nav2-bringup
 
 # colcon
